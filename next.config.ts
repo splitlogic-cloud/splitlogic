415c96cffbf44482cb8f951a82d20f63beb927a1
Erik DittEfternamn <din_riktiga_email@domain.com>
2026-03-09 13:55:59 +0100

    increase server action body size limit

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+};
+
+export default nextConfig;
